9ad56d81b8a76685d623555c81c9b0bbd13025e0
julian.escobar <julian.escobar@pragma.com.co>
2017-05-29 11:25:32 -0500

    Modifico estilo en en registrar usuario

---
+++ Login/Registrar_usuario.js
@@ -0,0 +1,60 @@
+(function() {
+    'use strict';
+
+    angular
+    .module('starter',['ngMaterial'])
+    .controller('registrarUsuarioController', registrarUsuarioController);
+ //    registrarUsuarioController.$inject = ['registarUsuarioServices'];
+
+    function registrarUsuarioController($scope, $mdDialog,registarUsuarioServices) {
+        var vm = this;
+        vm.registrarUsuario = registrarUsuario;
+
+
+     function registrarUsuario() { 
+
+                 var requestJson = {
+                    "nombre1" : vm.nombre1,
+                    "nombre2" : vm.nombre2,
+                    "apellido1" : vm.apellido1,
+                    "apellido2" : vm.apellido2,
+                    "telefonoFijo" : vm.telefonoFijo,
+                    "telefonomovil" : vm.telefonoMovil, 
+                    "email" : vm.email,
+                    "pregunta":  vm.preguntaSeguridad,
+                    "respuesta" :  vm.respuesta,
+                    "contrasena" :vm.contrasena,
+                    "confirmarContrasena" : vm.confirmarContrasena
+                    }        
+             vm.modalShown2 = true;
+               console.log(JSON.stringify(requestJson));
+
+              registarUsuarioServices.registrarUsuario(requestJson).then(function(data){
+                debugger;
+            if(data.resultado.codRespuesta == "200") {                
+                $mdDialog.show(
+                  $mdDialog.alert()
+                     .parent(angular.element(document.querySelector('#dialogContainer')))
+                     .clickOutsideToClose(true)
+                     .title('Registrar usuario')
+                     .textContent('Usuario registrado')
+                     .ariaLabel('Usuario registrado')
+                     .ok('Cerrar')                     
+               );
+            }else{
+                $mdDialog.show(
+                  $mdDialog.alert()
+                     .parent(angular.element(document.querySelector('#dialogContainer')))
+                     .clickOutsideToClose(true)
+                     .title('Registrar usuario')
+                     .textContent('Usuario no registrado')
+                     .ariaLabel('Usuario no registrado')
+                     .ok('Cerrar')
+                     
+               );
+            }  
+       });
+   }
+}        
+    
+})();
