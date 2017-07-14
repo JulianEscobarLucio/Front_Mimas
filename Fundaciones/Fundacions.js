652d78c926f6c645d09aa2bd02c8f428552580c8
Mario Andrés Ruiz <marioarb.97@gmail.com>
2017-07-13 22:02:53 -0500

    se añade estadoEntidad
    activar/ inactivar fundacion

--- Fundaciones/Fundacions.js
+++ Fundaciones/Fundacions.js
@@ -4,14 +4,16 @@
     angular
     .module('starter',['ngMaterial'])
     .controller('fundacionController', fundacionController);
-    //debugger;
+    
     // fundacionController.$inject = ['fundacionServices'];
 
     function fundacionController($scope, $mdDialog,fundacionServices) {
-        //debugger;
+        
 
         localStorage.setItem('userID', 'Mario');
         var user = localStorage.getItem('userID');
+        
+
 
         var vm = this;
         
@@ -19,7 +21,7 @@
         vm.registrarFundacion = registrarFundacion;
         vm.actualizarFundacion = actualizarFundacion;
         vm.consultarFundacion = consultarFundacion;
-        vm.eliminarFundacion = confirmar;
+        //vm.eliminarFundacion = confirmar; no se está usando
         vm.registrarDisabled = false;
         vm.consultarDisabled = false; 
         vm.actualizarDisabled = true;
@@ -28,6 +30,8 @@
         //obtener usuario del local storage
         vm.usuarioDisabled = true;
         vm.usuario = user;
+        vm.estado = 'Activa';
+        vm.fnEstado = fnEstado;        
         
 
 
@@ -48,6 +52,7 @@
         vm.msjUsuario = "";
         vm.fnTipoEntidad = fnTipoEntidad;
         vm.msjTipoEntidad = "";
+        
 
 
         vm.fnIdentificacion2 = fnIdentificacion2;
@@ -102,10 +107,58 @@
             }     
 
         }
+        function fnTipoEntidad(){
+            if(vm.tipoEntidad != undefined || vm.tipoEntidad != ''){
+              vm.msjTipoEntidad = "";
+            }     
+
+        } 
 
 
 
-        
+
+      function  fnEstado(ev) {
+          
+          if (vm.estado == 'Activa') {
+
+            var confirm = $mdDialog.confirm()
+                .title('Cambiar Estado')
+                .textContent('¿Está seguro de activar la fundación?')
+                .ariaLabel('Lucky day')
+                .targetEvent(ev)
+                .ok('Activar Fundación')
+                .cancel('Cancelar');
+
+            $mdDialog.show(confirm).then(function() {
+              vm.estado = 'Activa';
+            }, function() {
+              vm.estado = 'Inactiva';
+              return;
+            });
+
+          }else if (vm.estado == 'Inactiva') {
+            
+            var confirm = $mdDialog.confirm()
+                .title('Cambiar Estado')
+                .textContent('¿Está seguro de inactivar la fundación?')
+                .ariaLabel('Lucky day')
+                .targetEvent(ev)
+                .ok('Inactivar Fundación')
+                .cancel('Cancelar');
+
+            $mdDialog.show(confirm).then(function() {
+              vm.estado = 'Inactiva';
+            }, function() {
+              vm.estado = 'Activa';
+              return;
+            });
+            
+          }
+          
+        }
+
+
+
       function camposObligatorios(){
 
         //validaciones campos obligatorios
@@ -135,7 +188,7 @@
           else if(vm.usuario == undefined || vm.usuario == '' ){
               vm.msjUsuario  = "Debes ingresar un valido para este campo";
               return false;
-          }else if(vm.tipoEntidad == undefined || vm.tipoEntidad == '' || vm.tipoEntidad == '1' ){
+          }else if(vm.tipoEntidad == undefined || vm.tipoEntidad == '' || vm.tipoEntidad == '1' || vm.tipoEntidad == 'Seleccione una Entidad.' ){
               vm.msjTipoEntidad  = "Debes ingresar un valido para este campo";
               return false;
           }
@@ -151,46 +204,14 @@
           vm.email = "";
           vm.direccion = "";
           vm.usuario = localStorage.getItem('userID');
-          vm.tipoEntidad = "";
+          vm.tipoEntidad = "1";
+          vm.estado = 'Activa';
       }
-
-      function confirmar (){
-
-        if (confirm('¿Estas seguro de eliminar esta Fundación?')){ 
-           
-            eliminarFundacion();
-
-            limpiarCampos();
-            vm.identificacion2 = "";
-
-            vm.DisabledIdentificacion2  = false;
-            vm.DisabledIdentificacion  = false;
-            vm.registrarDisabled = false;
-            vm.consultarDisabled = false; 
-            vm.actualizarDisabled = true;
-            vm.eliminarDisabled = true;
-
-        } 
-        else{
-
-          limpiarCampos();
-          vm.identificacion2 = "";
-          vm.DisabledIdentificacion2  = false;
-          vm.DisabledIdentificacion  = false;
-          vm.registrarDisabled = false;
-          vm.consultarDisabled = false; 
-          vm.actualizarDisabled = true;
-          vm.eliminarDisabled = true; 
-
-          return;
-        }
-      }
-
 
 
      function registrarFundacion() { 
 
-      //debugger;
+      
 
         vm.camposObligatorios = camposObligatorios();
         if(vm.camposObligatorios == false){
@@ -205,7 +226,8 @@
                     "email" : vm.email,
                     "direccion" : vm.direccion,
                     "usuario" :vm.usuario,
-                    "tipoEntidad" :vm.tipoEntidad
+                    "tipoEntidad" :vm.tipoEntidad,
+                    "estadoEntidad" : vm.estado
                     }  
                  
                  
@@ -213,26 +235,8 @@
                console.log(JSON.stringify(requestJson));
 
 
-            fundacionServices.consultarFundacion(requestJson).then(function(data){
-
-              if(data.resultado[0].codRespuesta == "200") {
-                $mdDialog.show(
-                   $mdDialog.alert()
-                   .parent(angular.element(document.querySelector('#dialogContainer')))
-                   .clickOutsideToClose(true)
-                   .title('Error al registrar Fundación')
-                   .textContent('La Fundación ya existe')
-                   .ariaLabel('La Fundación ya existe')
-                   .ok('Cerrar')                     
-                  );
-
-                vm.actualizarDisabled = false;
-
-              }
-              else{
-
                 fundacionServices.registrarFundacion(requestJson).then(function(data){
-                //debugger;
+                
 
                 if(data.resultado[0].codRespuesta == "200") {                
                     $mdDialog.show(
@@ -250,7 +254,22 @@
                     vm.actualizarDisabled = true;
 
                            
-                }else{
+                }else if (data.resultado[0].codRespuesta == "204") {
+                    $mdDialog.show(
+                      $mdDialog.alert()
+                         .parent(angular.element(document.querySelector('#dialogContainer')))
+                         .clickOutsideToClose(true)
+                         .title('Error al Registrar Fundación')
+                         .textContent('La fundación ya se encuentra registrada')
+                         .ariaLabel('Fundación ya se encuentra registrada')
+                         .ok('Cerrar')
+                         
+                   );
+
+                    vm.actualizarDisabled = false;
+
+                } 
+                else {
                     $mdDialog.show(
                       $mdDialog.alert()
                          .parent(angular.element(document.querySelector('#dialogContainer')))
@@ -261,11 +280,11 @@
                          .ok('Cerrar')
                          
                    );
-                }  
+                } 
               }); //fin fundacionServices.registrarFundacion
 
-        }
-      }); //fin fundacionServices.consultarFundacion
+        //}
+      //});//fin fundacionServices.consultarFundacion
 
    }
      
@@ -289,11 +308,12 @@
                     "email" : vm.email,
                     "direccion" : vm.direccion,
                     "usuario" :vm.usuario,
-                    "tipoEntidad" :vm.tipoEntidad
+                    "tipoEntidad" :vm.tipoEntidad,
+                    "estadoEntidad" :vm.estado
                     }  
     	 
     	   fundacionServices.actualizarFundacion(requestJson).then(function(data){
-           //debugger;
+           
            
            if(data.resultado[0].codRespuesta == "200") {     
                   $mdDialog.show(
@@ -353,7 +373,8 @@
                     "email" : vm.email,
                     "direccion" : vm.direccion,
                     "usuario" :vm.usuario,
-                    "tipoEntidad" :vm.tipoEntidad
+                    "tipoEntidad" :vm.tipoEntidad,
+                    "estadoEntidad" :vm.estado
                     }  
 
          fundacionServices.consultarFundacion(requestJson).then(function(data){
@@ -378,14 +399,15 @@
                  vm.email = data.resultado[0].email,
                  vm.direccion = data.resultado[0].direccion,
                  vm.usuario = data.resultado[0].usuario,
-                 vm.tipoEntidad = data.resultado[0].tipoEntidad
+                 vm.tipoEntidad = data.resultado[0].tipoEntidad,
+                 vm.estado = data.resultado[0].estadoEntidad
 
                  vm.DisabledIdentificacion2  = true;
                  vm.DisabledIdentificacion  = true;
                  vm.actualizarDisabled = false;
                  vm.eliminarDisabled = false;
                  vm.registrarDisabled = true;
-                 vm.consultarDisabled = true; 
+                 vm.consultarDisabled = false; 
 
 
             }else {
@@ -406,6 +428,36 @@
 
 
 
+
+
+    $scope.showConfirm = function(ev) {
+          // Appending dialog to document.body to cover sidenav in docs app
+          var confirm = $mdDialog.confirm()
+                .title('Eliminar Fundación')
+                .textContent('¿Está seguro de eliminar esta Fundación?')
+                .ariaLabel('Lucky day')
+                .targetEvent(ev)
+                .ok('Si, deseo eliminar la Fundación')
+                .cancel('Cancelar');
+
+          $mdDialog.show(confirm).then(function() {
+            eliminarFundacion();
+          }, function() {
+
+              limpiarCampos();
+              vm.identificacion2 = "";
+              vm.DisabledIdentificacion2  = false;
+              vm.DisabledIdentificacion  = false;
+              vm.registrarDisabled = false;
+              vm.consultarDisabled = false; 
+              vm.actualizarDisabled = true;
+              vm.eliminarDisabled = true;
+            return;
+          });
+        };
+
+
+
     function eliminarFundacion(){
      
 
@@ -417,7 +469,8 @@
                     "email" : vm.email,
                     "direccion" : vm.direccion,
                     "usuario" :vm.usuario,
-                    "tipoEntidad" :vm.tipoEntidad
+                    "tipoEntidad" :vm.tipoEntidad,
+                    "estadoEntidad" :vm.estado
                     }  
 
          fundacionServices.eliminarFundacion(requestJson).then(function(data){
@@ -433,6 +486,16 @@
                    .ok('Cerrar')                     
                   ); 
 
+              limpiarCampos();
+              vm.identificacion2 = "";
+
+              vm.DisabledIdentificacion2  = false;
+              vm.DisabledIdentificacion  = false;
+              vm.registrarDisabled = false;
+              vm.consultarDisabled = false; 
+              vm.actualizarDisabled = true;
+              vm.eliminarDisabled = true;
+
             }else {
                   $mdDialog.show(
                     $mdDialog.alert()
@@ -443,6 +506,15 @@
                    .ariaLabel('Fundación no eliminada')
                    .ok('Cerrar')                     
                   );
+
+              limpiarCampos();
+              vm.identificacion2 = "";
+              vm.DisabledIdentificacion2  = false;
+              vm.DisabledIdentificacion  = false;
+              vm.registrarDisabled = false;
+              vm.consultarDisabled = false; 
+              vm.actualizarDisabled = true;
+              vm.eliminarDisabled = true; 
 
             }            
          });
